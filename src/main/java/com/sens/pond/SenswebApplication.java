c0a3045022f4b537c2c2d7a6c82a659e246472d2
senspond20 <senspond@gmail.com>
2021-02-15 11:11:38 +0900

    CommandLineRunner Test

--- src/main/java/com/sens/pond/SenswebApplication.java
+++ src/main/java/com/sens/pond/SenswebApplication.java
@@ -10,20 +10,55 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SenswebApplication {
+	@Autowired 
+	private BoardRepository boardRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SenswebApplication.class, args);
 	}
 
+	// https://jeong-pro.tistory.com/206
+	// component scanning에 의한 방식으로 빈을 등록하는 방법
+	@Component 
+	class MyCLRunner implements { 
+		@Override public void run(String... args) throws Exception { 
+			int count = 154;
+			long beforeTime = System.currentTimeMillis(); 
+			IntStream.rangeClosed(1, count).forEach(i -> {
+				Board board = Board.builder()
+						.title("test" + i)
+						.content("내용입니다." + i)
+						.author("작성자" + i)
+						.build();
+				boardRepository.save(board);
+			});
+			long afterTime = System.currentTimeMillis(); 
+			System.out.println("@@ Insert Board" + count + "건 수행 시간 : " + (afterTime - beforeTime) + "ms");
+		} 
+	}
+
 	/**
      * 	샘플 데이터 
 	 **/
-    @Bean
+ /*   @Bean
     public CommandLineRunner initData(@Autowired BoardRepository boardRepository) {
-        return args -> 
+		long beforeTime = System.currentTimeMillis(); 
+		IntStream.rangeClosed(1, 154).forEach(i -> {
+			Board board = Board.builder()
+					.title("test" + i)
+					.content("내용입니다." + i)
+					.author("작성자" + i)
+					.build();
+			boardRepository.save(board);
+		});
+		long afterTime = System.currentTimeMillis(); 
+		System.out.println("@@ 수행 시간 : " + (afterTime - beforeTime));
+		
+        return args -> 	
             IntStream.rangeClosed(1, 154).forEach(i -> {
 
                 Board board = Board.builder()
@@ -33,6 +68,6 @@ public class SenswebApplication {
                         .build();
                 boardRepository.save(board);
             });
-    }
+    }*/
 
 }
